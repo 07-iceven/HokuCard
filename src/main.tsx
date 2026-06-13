24908bee1c1a9662ba1551c405ebe499b40d933e
iceven <iceven@outlook.jp>
2026-06-13 19:23:04 +0800

    feat(style): 引入 Noto Serif SC 作为默认宋体字体
    - 安装 @fontsource/noto-serif-sc 依赖，实现字体本地化加载
    - 在 main.tsx 中全局引入 Noto Serif SC 的常用字重 (300, 400, 500, 700)
    - 更新 Tailwind CSS 的 --font-serif 变量，优先使用 Noto Serif SC
    - 将印章样式 (.hanko-stamp) 的字体替换为全局 serif 变量

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import '@fontsource/noto-serif-sc/300.css';
+import '@fontsource/noto-serif-sc/400.css';
+import '@fontsource/noto-serif-sc/500.css';
+import '@fontsource/noto-serif-sc/700.css';
 import App from './App.tsx';
 import './index.css';
 
